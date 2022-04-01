55d3656f94b676212dfbf5e3515c6cca5b68dcc1
FinallRareStaff <Jeymester@gmail.com>
2022-04-02 00:15:25 +0300

    re

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.connectDB();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/kata";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "EBA14c86a74gEBA";
+
+    public static Connection connectDB() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Подключение состоялось");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Подключение не состоялось");
+        }
+        return connection;
+
+    }
 }
