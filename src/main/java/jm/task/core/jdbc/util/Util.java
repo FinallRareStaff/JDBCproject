625a890a039e038f943bfd060fc17def48b9878b
FinallRareStaff <Jeymester@gmail.com>
2022-04-04 15:38:23 +0300

    Class Util reworked

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,29 +1,40 @@
 package jm.task.core.jdbc.util;
 
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Util {
     // реализуйте настройку соеденения с БД
     private static final String URL = "jdbc:mysql://localhost:3306/kata";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "EBA14c86a74gEBA";
+    private static Connection connection = null;
+    private static Statement statement = null;
 
     public static Connection connectDB() {
-        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            System.out.println("Подключение состоялось");
+            System.out.println("Connection success");
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-            System.out.println("Подключение не состоялось");
+            System.out.println("Connection not success");
         }
         return connection;
-
+    }
+    public static void closeConnectDB() {
+        try {
+            if (statement != null) {
+                statement.close();
+                System.out.println("Statement close");
+            }
+            if (connection != null) {
+                connection.close();
+                System.out.println("Connection close");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
